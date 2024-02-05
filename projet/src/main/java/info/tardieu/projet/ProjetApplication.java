a14efa7d78350fbb2477df958232bd600889ee40
Maxime TARDIEU <mtardieu@juniorisep.com>
2024-02-05 13:45:12 +0100

    init

---
+++ projet/src/main/java/info/tardieu/projet/ProjetApplication.java
@@ -0,0 +1,13 @@
+package info.tardieu.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetApplication.class, args);
+    }
+
+}
